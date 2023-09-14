583f1ed37ca7535039320ba8b101c81c30a2bfa8
omerfaruksen <sen.faruk.93@gmail.com>
2023-09-15 01:11:20 +0300

    Uçak Bileti Hesaplama

---
+++ UcakBiletiHesaplama/Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        int yas, km, select;
+        double normalTutar, yasIndirimi1, yasIndirimi2, yasIndirimi3, indirimliTutar1,indirimliTutar2,indirimliTutar3;
+
+
+        Scanner info=new Scanner(System.in);
+        System.out.print("Gidilecek mesafe (km): ");
+        km=info.nextInt();
+        System.out.print("Yaşınız? : ");
+        yas=info.nextInt();
+        System.out.print("Yolculuk tipini seçiniz: \n1-Tek Yön: \n2-Gidiş Dönüş: ");
+        select= info.nextInt();
+        
+
+        normalTutar= km *0.10;
+        yasIndirimi1=normalTutar*0.50;
+        yasIndirimi2=normalTutar*0.10;
+        yasIndirimi3=normalTutar*0.30;
+        indirimliTutar1=normalTutar-yasIndirimi1;
+        indirimliTutar2=normalTutar-yasIndirimi2;
+        indirimliTutar3=normalTutar-yasIndirimi3;
+        if (yas<=12 && select==1 && km>0){
+             System.out.print("Toplam Tutar: " +indirimliTutar1);
+        } else if (yas<=12 && select==2 && km>0) {
+            System.out.print("Toplam Tutar: " +((indirimliTutar1-(indirimliTutar1*0.20))*2));
+        } else if (yas>12 && yas<=24 && select==1 && km>0) {
+            System.out.print("Toplam Tutar: " +indirimliTutar2);
+        } else if (yas>12 && yas<=24 && select==2 && km>0) {
+            System.out.print("Toplam Tutar: " +((indirimliTutar2-(indirimliTutar2*0.20))*2));
+        } else if (yas>=65 && select==1 && km>0) {
+            System.out.print("Toplam Tutar: " +indirimliTutar3);
+        } else if (yas>=65 && select==2 && km>0) {
+            System.out.print("Toplam Tutar: " +((indirimliTutar3-(indirimliTutar3*0.20))*2));
+        } else if (yas>24 && yas<65 && select==1 && km>0) {
+            System.out.print("Toplam Tutar: " +normalTutar);
+        } else if (yas>24 && yas<65 && select==2 && km>0) {
+            System.out.print("Toplam Tutar: " +normalTutar*0.20*2);
+        } else{
+            System.out.print("HATALI GİRİŞ YAPTINIZ!");
+        }
+
+
+    }
+}
