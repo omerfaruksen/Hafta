b4ee939346cbd8051fb3aef8c51824de2477fe18
omerfaruksen <sen.faruk.93@gmail.com>
2023-09-15 01:12:59 +0300

    Yorum Satırları eklendi

--- UcakBiletiHesaplama/Main.java
+++ UcakBiletiHesaplama/Main.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
+        //Değişkenleri tanımlıyoruz
         int yas, km, select;
         double normalTutar, yasIndirimi1, yasIndirimi2, yasIndirimi3, indirimliTutar1,indirimliTutar2,indirimliTutar3;
 
-
+        //Scanner sınıfı ile kullanıcıdan veri alıyoruz
         Scanner info=new Scanner(System.in);
         System.out.print("Gidilecek mesafe (km): ");
         km=info.nextInt();
@@ -12,8 +13,8 @@ public class Main {
         yas=info.nextInt();
         System.out.print("Yolculuk tipini seçiniz: \n1-Tek Yön: \n2-Gidiş Dönüş: ");
         select= info.nextInt();
-        
 
+        //Hesaplama ve formüllerimiz
         normalTutar= km *0.10;
         yasIndirimi1=normalTutar*0.50;
         yasIndirimi2=normalTutar*0.10;
@@ -21,6 +22,8 @@ public class Main {
         indirimliTutar1=normalTutar-yasIndirimi1;
         indirimliTutar2=normalTutar-yasIndirimi2;
         indirimliTutar3=normalTutar-yasIndirimi3;
+
+        //İf-Else koşullu ifadelerimiz
         if (yas<=12 && select==1 && km>0){
              System.out.print("Toplam Tutar: " +indirimliTutar1);
         } else if (yas<=12 && select==2 && km>0) {
