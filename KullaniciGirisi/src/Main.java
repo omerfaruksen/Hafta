206c0902be64c66f895e318e0f68864d6899530c
omerfaruksen <sen.faruk.93@gmail.com>
2023-09-15 02:57:38 +0300

    Kullanıcı girisi

--- KullaniciGirisi/src/Main.java
+++ KullaniciGirisi/src/Main.java
@@ -1,36 +1,38 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        String userName, password, newPassword;
-        int select;
-        Scanner inp=new Scanner(System.in);
+        String userName, password;
+
+        Scanner inp = new Scanner(System.in);
 
         System.out.print("Kullanıcı Adınız: ");
-        userName=inp.nextLine();
+        userName = inp.nextLine();
         System.out.print("Şifreniz: ");
-        password=inp.nextLine();
+        password = inp.nextLine();
 
-        if (userName.equals("Patika")&& password.equals("java123")){
-           System.out.println("Giriş Yaptınız!");
-        }else{
+        if (userName.equals("Patika") && password.equals("java123")) {
+            System.out.println("Giriş Yaptınız!");
+        } else {
             System.out.println("Bilgileriniz yalnış!");
             System.out.println("şifrenizi sıfırlamak ister misiniz) \n1-Evet\n2-Hayır");
-            select=inp.nextInt();
-
+            int select = inp.nextInt();
             if(select==1){
-                Scanner input=new Scanner(System.in);
+                Scanner input = new Scanner(System.in);
                 System.out.print("Yeni şifrenizi giriniz: ");
-                newPassword=inp.nextLine();
-                if(newPassword.equals(password)){
-                    System.out.print("Yeni Şifreniz eski şifreniz ile aynı olamaz!");
-                }else{
-                    System.out.println("Şifreniz oluşturuldu.");
-                }
-        }else{
-            System.out.println("Patika iyi günler diler.");
+                String newPassword = input.nextLine();
+
+
+                boolean kosul1=newPassword.equals(password);
+                String str=(kosul1)? "Yeni Şifreniz Oluşturuldu": "Yeni şifre eski şifre ile aynı olamaz.";
+                System.out.print(str);
+            }else {
+                System.out.print("Patika iyi günler diler!");
+            }
         }
 
-        }
+
+
+            }
 
     }
-}
+
