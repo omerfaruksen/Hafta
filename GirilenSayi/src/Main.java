5b1513732dad8d9932b59630ee31b7f19b4a89db
omerfaruksen <sen.faruk.93@gmail.com>
2023-09-15 16:35:26 +0300

    güncelleme

--- GirilenSayi/src/Main.java
+++ GirilenSayi/src/Main.java
@@ -1,16 +1,17 @@
-import java.sql.SQLOutput;
+
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
-
+        //Değişkenleri tanımlayalım
         int k, total=0, sayac=0;
+        //Kullanıcıdan veri alalım
         Scanner info=new Scanner(System.in);
         System.out.print("Bir Sayi Giriniz :");
         k= info.nextInt();
-
+        //Döngümüzü başlatalım
         for (int i=1; i<=k; i++){
             if (i%3==0 && i%4==0) {
-                //System.out.print(i +",");
+                
                 total=total+i;
                 sayac=sayac+1;
             }
